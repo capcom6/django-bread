84f0bdc6a585e58493bd8c26832815b4bcf53678
Aleksandr Soloshenko <capcom2me@gmail.com>
2022-06-27 22:02:27 +0700

    Improved: optimize queries on admin page

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,8 @@
+from typing import Any
 from django.contrib import admin
+from django.forms import ModelChoiceField
+from django.http import HttpRequest
+from django.db.models import ForeignKey
 
 from .models import *
 
@@ -6,24 +10,29 @@ from .models import *
 class RecipeIngredientAdminInline(admin.TabularInline):
     model = RecipeIngredient
 
+    def __init__(self, parent_model, admin_site) -> None:
+        super().__init__(parent_model, admin_site)
+        self.all_ingredients = Ingredient.objects.all()
+        self.all_measures = Measure.objects.all()
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("ingredient", "measure")
+
+
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_filter = ("category",)
     inlines = (RecipeIngredientAdminInline,)
-
-
-admin.site.register(Recipe, RecipeAdmin)
 
 
 class MeasureWeightAdminInline(admin.TabularInline):
     model = MeasureWeight
 
 
+@admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     inlines = (MeasureWeightAdminInline,)
 
-
-admin.site.register(Ingredient, IngredientAdmin)
 
 # Register your models here.
 admin.site.register([Measure, MeasureWeight, Program, RecipeIngredient, Category])
